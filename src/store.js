b36a6a3d5dc3a486eb5764a273e50dfea439c8b5
bigmosi <kinyeramo@gmail.com>
2022-10-19 18:18:17 +0300

    setting up the store

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import messageReducer from '../features/message/messageSlice';
+
+export const store = configureStore({
+  reducer: {
+    message: messageReducer
+  }
+});
